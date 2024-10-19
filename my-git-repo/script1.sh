59a1ddcf9094609936343c4b59425f05df82e22c
Manigandan Ravichandran <ab7028@student.jamk.fi>
2024-10-19 12:37:23 +0530

    Initial commit with script files

---
+++ my-git-repo/script1.sh
@@ -0,0 +1,6 @@
+e script1.sh script2.sh
+echo "#!/bin/bash" >> script1.sh
+echo "echo 'Hello from script1'" >> script1.sh
+echo "#!/bin/bash" >> script2.sh
+echo "echo 'Hello from script2'" >> script2.sh
+ho 'Hello from script1'
